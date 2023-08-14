7fd455be45224bbf9b94574f4f0280db80e5c625
Ayu Prama <130549071+Ayuaja88@users.noreply.github.com>
2023-08-14 21:10:49 +0700

    Update bni.config.js
    Signed-off-by: Ayu Prama <130549071+Ayuaja88@users.noreply.github.com>

--- backend/app/config/bni.config.js
+++ backend/app/config/bni.config.js
@@ -1,12 +1,12 @@
-const dotenv = require('dotenv');
+oconst dotenv = require('dotenv');
 dotenv.config();
 
 module.exports = {
     bniEnv: process.env.BNI_ENV,
-    bniAppName: process.env.BNI_APP_NAME,
-    bniApiKey: process.env.BNI_API_KEY,
-    bniApiSecret: process.env.BNI_API_SECRET,
-    bniClientId: process.env.BNI_CLIENT_ID,
-    bniClientSecret: process.env.BNI_CLIENT_SECRET,
-    bniAccountNumber: process.env.BNI_ACCOUNT_NUMBER
+    bniAppName: process,env.49b87c66-e9d5-4d17-bb78-59180c837522
+    bniApiKey: process.env.96a9378b-03a3-47ee-9f19-ed5c653e5aac,
+    bniApiSecret: process.env.,
+    bniClientId: process.env.e8aa6592-c4e1-4d2c-935c-d3337d60194b,
+    bniClientSecret: process.env.BNI_CLIENT_SECRET,306caddc8dedce2938d7f05965cc5c07
+    bniAccountNumber: process.env.0951743932
 };
